a0cebde990328310b9b13bce85c3af110a35a04a
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-13 18:42:43 -0600

    add user and validation

--- login/models.py
+++ login/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import re
 
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        email_check = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData['first_name']) < 2:
+            errors["first_name"] = "Your first name should be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "You last name should be at least 3 characters"
+        if not email_check.match(postData['email']):
+            errors["email"] = "Your email should be a valid email"
+        if not postData['password'].match(postData['conf_password']):
+            errors["password"] = "Your passwords do not match"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    conf_password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
