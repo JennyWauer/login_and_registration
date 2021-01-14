e5a55f7155db96b2bbfe043958f41636aebf5635
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-14 09:57:36 -0600

    update validation and validation display

--- login/models.py
+++ login/models.py
@@ -6,13 +6,17 @@ class UserManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
         email_check = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        password = postData['password']
+        conf_password = postData['conf_password']
         if len(postData['first_name']) < 2:
             errors["first_name"] = "Your first name should be at least 2 characters"
         if len(postData['last_name']) < 2:
             errors["last_name"] = "You last name should be at least 3 characters"
         if not email_check.match(postData['email']):
             errors["email"] = "Your email should be a valid email"
-        if not postData['password'].match(postData['conf_password']):
+        if len(postData['password']) < 8:
+            errors["password_len"] = "Your password should be at least 8 characters" 
+        if not password == conf_password:
             errors["password"] = "Your passwords do not match"
         return errors
 
