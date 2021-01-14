a6ac4d9ae7826bdb2f4652a3482b3410a051ee46
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-13 18:22:24 -0600

    add initial form action

--- login/views.py
+++ login/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def home(request):
-
     return render(request, 'home.html')
 
 def success(request):
     return render(request, 'success.html')
+
+def register(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        return redirect('/success')
